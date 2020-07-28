82a85abc608d42e6021815663892a5f4cf7a5d29
Ruben <rbnsilo@gmail.com>
2020-07-27 21:42:47 -0500

    Cambiamos el host de las websockets en produccion

--- realtime/client.js
+++ realtime/client.js
@@ -1,7 +1,14 @@
 //Aqui defino la logica del cliente que se va a comunicar con el servidor
 const io = require('socket.io-client');
 
-let socket = io.connect('http://localhost:3000', {reconnect: true});// Primer arguemnto el server y el sefundo argumento el json de config. EN este caso coloque que quierp que se reconecte en caso de que se reconecte
+let host = 'http://localhost:3000';
+
+
+if(process.env.NODE_ENV && process.env.NODE_ENV == 'production'){//En process.env se especifica el entorno en el que se esta ejecutando nuestra aplicacion. Si esta variable existe y su valor es igual a production, quiero que haga tal cosa
+  host = 'https://fathomless-fortress-68388.herokuapp.com/'
+}
+
+let socket = io.connect(host, {reconnect: true});// Primer arguemnto el server y el sefundo argumento el json de config. EN este caso coloque que quierp que se reconecte en caso de que se reconecte
 
 //La socket tiene un evento connect q se ejecuta cuando la conexion con web sockets finalizo correctamente
 socket.on('connect', function(){//Evento connect
